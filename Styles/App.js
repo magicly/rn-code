8c4df111ccfc6b796536e8407de91d534b10a101
magicly <magicly007@gmail.com>
2017-11-08 16:39:55 +0800

    styles

---
+++ Styles/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { StyleSheet, ActivityIndicator, Dimensions, SectionList, FlatList, ScrollView, Alert, Button, Text, View, Image, TextInput } from 'react-native';
+
+const MyText = ({ style, children }) => {
+  return <Text style={[{ fontFamily: 'Cochin', fontSize: 30 }, style]}>{children}</Text>
+}
+
+const TestImageResizeMode = ({ mode }) => {
+  return <Image
+    style={{
+      width: 150,
+      height: 150,
+      backgroundColor: 'green',
+      borderColor: 'red',
+      borderWidth: 1,
+      opacity: 0.9,
+      overflow: 'hidden',
+      resizeMode: mode,
+    }}
+    source={{ uri: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510640671&di=b92b670e8bfc393a334c7fa46375a9f4&imgtype=jpg&er=1&src=http%3A%2F%2Fi1.mopimg.cn%2Fimg%2Fdzh%2F2015-10%2F202%2F20151008002412502.JPEG' }}
+  />
+}
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.width = Dimensions.get('window').width;
+    this.height = Dimensions.get('window').height;
+  }
+  render() {
+    // Alert.alert(Object.keys(Image.resizeMode).join(' '));
+    return (
+      <ScrollView style={{
+        marginTop: 20, width: 300, height: 200,
+        borderColor: 'red',
+        borderWidth: 1,
+        borderRadius: 5,
+      }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 20, }}>
+          <TestImageResizeMode mode="contain" />
+          <TestImageResizeMode mode="cover" />
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 20, }}>
+          <TestImageResizeMode mode="stretch" />
+          <TestImageResizeMode mode="center" />
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 20, }}>
+          <TestImageResizeMode mode="repeat" />
+        </View>
+
+        <View style={{
+          position: 'absolute',
+          bottom: 10,
+          right: 20,
+          marginLeft: 10,
+          width: 80,
+          height: 50,
+          backgroundColor: 'hsla(360, 100%, 100%, 0.50)',
+          borderRadius: 5,
+          borderColor: 'green',
+          borderWidth: 1,
+          borderStyle: 'dotted',
+          opacity: 0.5,
+        }}><Text>position</Text></View>
+
+        <MyText>this is default text.</MyText>
+        <MyText style={{ color: 'red', fontSize: 40 }}>
+          this is a big red text!
+          <View style={{ width: 100, height: 30 }}>
+            <Text>this text has no style.</Text>
+          </View>
+          <Text>this text has styles.</Text>
+        </MyText>
+        <Text style={{ fontFamily: 'Cochin', fontSize: 15, fontWeight: 'bold', fontStyle: 'italic' }}>
+          base text
+          <Text style={{ fontSize: 20, fontStyle: 'normal' }}>nest text..</Text>
+          and this text is <Text style={{ color: 'red' }}>red!</Text>
+        </Text>
+
+        <Text>
+          There is a blue square
+          <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
+          in between my text.
+        </Text>
+        <Text style={{ fontStyle: 'italic' }}>
+          <Text>First part and </Text>
+          <Text>second part</Text>
+        </Text>
+        <View>
+          <Text>First part and </Text>
+          <Text>second part</Text>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
